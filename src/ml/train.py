b86202a722955bef5cb3407a63c16ad9d44ee1ff
huang2399 <2756813981@qq.com>
2026-04-15 18:18:22 +0800

    exp: 添加学习率调度器 Feature Flag 实验

--- src/ml/train.py
+++ src/ml/train.py
@@ -1,2 +1,10 @@
 import torch
 # 新模型：Transformer 实验
+
+# --- Feature Flag 模拟 ---
+USE_LR_SCHEDULER = True   # 设为 False 即可关闭新功能
+
+if USE_LR_SCHEDULER:
+    print("使用学习率调度器（实验性功能）")
+else:
+    print("使用默认学习率")
